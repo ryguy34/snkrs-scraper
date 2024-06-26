3cca00946026308eb2ed4143c5a817d76ac455af
Ryan <rymoran34@gmail.com>
2024-06-26 15:09:45 -0500

    Removed comment

--- src/main.ts
+++ src/main.ts
@@ -144,11 +144,11 @@ client.on("ready", async () => {
 	});
 
 	//runs every Thursday at 8PM
-	//cron.schedule("0 20 * * 4", async () => {
-	logger.info("Running Palace cron job");
-	await mainPalaceNotifications();
-	logger.info("Palace drops are done");
-	//});
+	cron.schedule("0 20 * * 4", async () => {
+		logger.info("Running Palace cron job");
+		await mainPalaceNotifications();
+		logger.info("Palace drops are done");
+	});
 
 	//runs everyday at 8PM
 	// cron.schedule("0 20 * * *", () => {
